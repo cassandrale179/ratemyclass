00b10ccbaff7a08bc33f9c70fa4a42123a4d8a58
Minh Le <lenguyetminh1998@gmail.com>
2017-05-17 21:50:18 -0400

    SQL input
    Successfully stringify JSON object

--- class/sql-input.js
+++ class/sql-input.js
@@ -4,6 +4,15 @@ courseRequest.onload = function(){
   var data = JSON.parse(courseRequest.responseText);
   var firstline = data["classes"][0];
   var string = Object.values(firstline);
-  console.log(string); 
+  string = JSON.stringify(string);
+  string = string.substring(2,string.length-2);
+  var split = string.split(" ");
+  console.log(split);
+
+  var title = split[0]+split[1];
+  var credit = split[split.length-2];
+
+
+
 }
 courseRequest.send();
