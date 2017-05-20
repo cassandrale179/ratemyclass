ab23a5ed340627b66e101195faeeecaacddd106a
Minh Le <lenguyetminh1998@gmail.com>
2017-05-19 20:23:41 -0400

    Update

--- text/sql-input.js
+++ text/sql-input.js
@@ -6,9 +6,9 @@ courseRequest.open('GET', 'allclasses/Antoinette.json', true);
 courseRequest.onload = function(){
   var data = JSON.parse(courseRequest.responseText);
 
-  //------CONVERTING EVEN NUMBER LINES INTO STRING------
-  for (var j = 0; j < 1644; j+=2)
-  {
+  for (var j = 0; j < 1644; j++){
+
+    //------ EVEN NUMBER LINES ARE CLASS TITLE ------
     if (j % 2 == 0){
       var string = JSON.stringify(Object.values(data["classes"][j]));
       string = string.substring(2,string.length-2);
@@ -23,20 +23,17 @@ courseRequest.onload = function(){
         var credit = arr[arr.length-2];
     }
 
+    //------ ODD NUMBER LINES ARE COURSE DESCRIPTION ------
     if(j % 2 != 0){
-        //------CONVERTING ODD NUMBER LINES INTO STRING------
         var str = JSON.stringify(Object.values(data["classes"][j]));
         var description = str.substring(2,str.length-2);
+        //------DISPLAY THE RESULT------
+        var between = "','";
+        var college = "A";
+        var SQLtext = "INSERT INTO course(class, title, description, credit, college) VALUES ('" + courseID + between + title + between + description + between + credit + between + college + "');<br>";
+        output.insertAdjacentHTML('beforeend', SQLtext);
 
     }
-
-    //------DISPLAY THE RESULT------
-
-    var between = "','";
-    var college = "A";
-    var SQLtext = "INSERT INTO course(class, title, description, credit, college) VALUES ('" + courseID + between + title + between + credit + between + college + "');<br>";
-    output.insertAdjacentHTML('beforeend', SQLtext);
-
   }
 }
 courseRequest.send();
